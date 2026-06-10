2141d1ffd4a14692898b59616801d864d6e5e6e2
MuthulakshmiQA-tech <muthulakshmimaheshwaran@gmail.com>
2026-06-10 21:07:47 +0100

    Add files via upload
    Day3_assignment

---
+++ numberType.js
@@ -0,0 +1,16 @@
+function number(){
+    let num=-1;
+    if(num>0)
+    {
+        console.log("Number is positive", num)
+    }
+    else if (num<0)
+    {
+        console.log("Number is negative", num);
+    }
+    else {
+        console.log("Number is zero", num);
+    
+    }
+}
+number();
