a324af161068d4f3eb1b31b3a66e7aea7e7e15b4
MuthulakshmiQA-tech <muthulakshmimaheshwaran@gmail.com>
2026-06-10 19:43:27 +0100

    Add files via upload
    Day1 Primitive DataTypes

---
+++ primitiveDataTypes.js
@@ -0,0 +1,10 @@
+let userName='Lakshmi';
+let organization='TestLeaf';
+let contactNumber=123456789;
+let knowsAutomation=true;
+let usesPlaywright;
+console.log(typeof userName);
+console.log(typeof organization)
+console.log(typeof contactNumber);
+console.log(typeof knowsAutomation);
+console.log(typeof usesPlaywright);
