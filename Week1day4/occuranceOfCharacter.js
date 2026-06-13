24f1b4e0eadf19fb5818468af38105a8a39bbab7
MuthulakshmiQA-tech <muthulakshmimaheshwaran@gmail.com>
2026-06-13 22:37:47 +0100

    Update occuranceOfCharacter.js

--- Week1day4/occuranceOfCharacter.js
+++ Week1day4/occuranceOfCharacter.js
@@ -1,2 +1,7 @@
-let str="Javascript";
-console.log(count(a));
+function countCharWithSplit(str, charToFind) {
+    // Splitting by the character breaks the string into pieces
+    return str.split(charToFind).length - 1;
+}
+
+// Example usage:
+console.log(countCharWithSplit("javascript", "a"));
